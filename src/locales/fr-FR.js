d1d3c22608bbf16183eb81519cbab7cd141275ed
Dominic Tobias <dominic.tobias@gmail.com>
2019-01-16 21:52:00 +0000

    Updated value from "Lorem ipsum dolor sit amet, consectetur adipiscing..." to "Lorem ipsum dolor sit amet, consectetur adipiscing..."

--- src/locales/fr-FR.js
+++ src/locales/fr-FR.js
@@ -4,7 +4,7 @@ const entries = {
   "lychee": "Are pretty great",
   "global-fwd": "Go forward___--",
   "global-back": "Le new value :)\n\n\nSpace!\nMore Space!",
-  "rather.long": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit volptpat lorem, eu viverra neque tristique sit amet. Vestibulum vulputate nunc vitae auctor ornare. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent blandit leo quis faucibus cursus.",
+  "rather.long": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit volptpat lorem, eu viverra neque tristique sit amet. Vestibulum vulputate nunc vitae auctor ornare. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesente blandit leo quis faucibus cursus.",
   "test-delete": "true some edit",
   "someNested": {
     "value": "Hi",
