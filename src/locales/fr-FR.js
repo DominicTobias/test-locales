b93fe41bb94c44155a0550f9765c9a8b89a410fc
Dominic Tobias <dominic.tobias@gmail.com>
2019-01-18 10:12:56 +0000

    Update fr-FR.js

--- src/locales/fr-FR.js
+++ src/locales/fr-FR.js
@@ -2,6 +2,7 @@
 const entries = {
   "lychee": "Are pretty greatez",
   "global-fwd": "Go forward___--",
+  "global-back": "testing delete",
   "rather.long": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit volptpat lorem, eu viverra neque tristique sit amet. Vestibulum vulputate nunc vitae auctor ornare. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesente blandit leo quis faucibus cursus.",
   "test-delete": "true some edit",
   "someNested": {
