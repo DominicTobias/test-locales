fbee067219b240603daa6b2805cfd85ad04e7b9a
Dominic Tobias <dominic.tobias@gmail.com>
2019-01-18 17:37:56 +0000

    Remove global-back entry

--- src/locales/fr-FR.js
+++ src/locales/fr-FR.js
@@ -1,7 +1,6 @@
 /* eslint disable */
 const entries = {
   "lychee": "Are pretty greatez",
-  "global-back": "testing delete",
   "rather.long": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit volptpat lorem, eu viverra neque tristique sit amet. Vestibulum vulputate nunc vitae auctor ornare. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesente blandit leo quis faucibus cursus.",
   "test-delete": "true some edit",
   "someNested": {
