e39bcfdbffbf12c99d28a78c60e53667832454a3
Dominic Tobias <dominic.tobias@gmail.com>
2019-01-20 19:05:52 +0000

    Update fr-FR.js

--- src/locales/fr-FR.js
+++ src/locales/fr-FR.js
@@ -3,6 +3,9 @@ const entries = {
   "lychee": "Are pretty greatez",
   "rather.long": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit volptpat lorem, eu viverra neque tristique sit amet. Vestibulum vulputate nunc vitae auctor ornare. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesente blandit leo quis faucibus cursus.",
   "test-delete": "true some edit",
+  "cSomething": "yeahc",
+  "aSomething": "yeaha",
+  "bSomething": "yeahb",
   "someNested": {
     "value": "Hi",
     "andArray": [
