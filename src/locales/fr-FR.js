c11e7707ebb880972a22c4404ff24f37a55f8644
Dominic Tobias <dominic.tobias@gmail.com>
2019-01-20 20:36:52 +0000

    Remove someNested→value entry

--- src/locales/fr-FR.js
+++ src/locales/fr-FR.js
@@ -1,13 +1,12 @@
 /* eslint disable */
 const entries = {
   "lychee": "Are pretty greatez",
-  "rather.long": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit volptpat lorem, eu viverra neque tristique sit amet. Vestibulum vulputate nunc vitae auctor ornare. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesente blandit leo quis faucibus cursus.",
-  "test-delete": "true some edit",
-  "cSomething": "yeahc",
   "aSomething": "yeaha",
   "bSomething": "yeahb",
+  "cSomething": "yeahc",
+  "rather.long": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit volptpat lorem, eu viverra neque tristique sit amet. Vestibulum vulputate nunc vitae auctor ornare. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesente blandit leo quis faucibus cursus.",
+  "test-delete": "true some edit",
   "someNested": {
-    "value": "Hi",
     "andArray": [
       "Yes-Maybe",
       "Nope?"
