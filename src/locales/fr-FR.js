7399a1fbdd853552e52941944bf9c21a1aeec8c0
Dominic Tobias <dominic.tobias@gmail.com>
2019-01-20 21:15:51 +0000

    Remove aSomething entry

--- src/locales/fr-FR.js
+++ src/locales/fr-FR.js
@@ -1,7 +1,6 @@
 /* eslint disable */
 const entries = {
   "lychee": "Are pretty greatez",
-  "aSomething": "yeaha",
   "cSomething": "yeahc",
   "rather.long": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit volptpat lorem, eu viverra neque tristique sit amet. Vestibulum vulputate nunc vitae auctor ornare. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesente blandit leo quis faucibus cursus.",
   "test-delete": "true some edit",
