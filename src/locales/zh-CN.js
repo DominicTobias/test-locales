c6d0b447ac7a61967f58727a5036d384176b66d5
Dominic Tobias <dominic.tobias@gmail.com>
2019-02-07 22:13:53 +0000

    Create new locale zh-CN.js

---
+++ src/locales/zh-CN.js
@@ -0,0 +1,13 @@
+/* eslint disable */
+const entries = {
+  cSomething: 'yeahabc',
+  'rather.long': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit volptpat lorem, eu viverra neque tristique sit amet. Vestibulum vulputate nunc vitae auctor ornare. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent blandit leo quis faucibus cursus.',
+  someNested: {
+    andArray: [
+      'Yes-Maybe',
+      'Enter'
+    ]
+  }
+};
+
+export default entries;
