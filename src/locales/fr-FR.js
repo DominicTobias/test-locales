21df6cb74571c42c5908cd2c1d6bb9888d97608e
Dominic Tobias <dominic.tobias@gmail.com>
2019-02-13 22:39:15 +0000

    Created "country→3" with value "Another one!?"

--- src/locales/fr-FR.js
+++ src/locales/fr-FR.js
@@ -4,7 +4,8 @@ const entries = {
   lychee: 'Are pretty greatez',
   country: [
     'Allemagne',
-    'Nope?'
+    'Nope?',
+    'Another one!?'
   ],
   cSomething: 'yeahc',
   'rather.long': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit volptpat lorem, eu viverra neque tristique sit amet. Vestibulum vulputate nunc vitae auctor ornare. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesente blandit leo quis faucibus cursus.',
