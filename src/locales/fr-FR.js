f9110c9d9d00704064cb55ccd671817c7bc7c351
Dominic Tobias <dominic.tobias@gmail.com>
2019-02-13 22:37:44 +0000

    Updated key from "someNested→andArray→0" to "country→0". Updated value from "Yes-Maybe" to "Allemagne"

--- src/locales/fr-FR.js
+++ src/locales/fr-FR.js
@@ -2,13 +2,15 @@
 const entries = {
   boBble: 'hat',
   lychee: 'Are pretty greatez',
+  country: [
+    'Allemagne'
+  ],
   cSomething: 'yeahc',
   'rather.long': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit volptpat lorem, eu viverra neque tristique sit amet. Vestibulum vulputate nunc vitae auctor ornare. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesente blandit leo quis faucibus cursus.',
   'test-delete': 'true some edit',
   'testing-height': 'Test le height',
   someNested: {
     andArray: [
-      'Yes-Maybe',
       'Nope?'
     ]
   }
