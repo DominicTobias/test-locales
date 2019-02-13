0a9c792588c8895e2d949f133709a7afc8288092
Dominic Tobias <dominic.tobias@gmail.com>
2019-02-13 22:37:53 +0000

    Updated key from "someNested→andArray→1" to "country→1"

--- src/locales/fr-FR.js
+++ src/locales/fr-FR.js
@@ -3,17 +3,13 @@ const entries = {
   boBble: 'hat',
   lychee: 'Are pretty greatez',
   country: [
-    'Allemagne'
+    'Allemagne',
+    'Nope?'
   ],
   cSomething: 'yeahc',
   'rather.long': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit volptpat lorem, eu viverra neque tristique sit amet. Vestibulum vulputate nunc vitae auctor ornare. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesente blandit leo quis faucibus cursus.',
   'test-delete': 'true some edit',
-  'testing-height': 'Test le height',
-  someNested: {
-    andArray: [
-      'Nope?'
-    ]
-  }
+  'testing-height': 'Test le height'
 };
 
 export default entries;
