9212d6ae3b97e25753c48084a94306f5b2f9e7a2
Dominic Tobias <dominic.tobias@gmail.com>
2019-02-24 22:53:56 +0000

    Remove only-for-french entry

--- src/locales/fr-FR.js
+++ src/locales/fr-FR.js
@@ -4,7 +4,6 @@ const entries = {
   'adding-lemon': 'citron',
   'adding-orange': 'citron',
   'keys-only-test': '',
-  'only-for-french': 'Should not appear in non-french',
   'copy-value-to-all': 'This should be the same in all languages'
 };
 
