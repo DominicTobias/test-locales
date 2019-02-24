ae5b1927b1b035493a9d0fe5db85e64312ab2e64
Dominic Tobias <dominic.tobias@gmail.com>
2019-02-24 22:44:04 +0000

    Add "keys-only-test": ""

--- src/locales/fr-FR.js
+++ src/locales/fr-FR.js
@@ -2,7 +2,8 @@
 const entries = {
   'adding-apple': 'Pomme',
   'adding-lemon': 'citron',
-  'adding-orange': 'citron'
+  'adding-orange': 'citron',
+  'keys-only-test': ''
 };
 
 export default entries;
