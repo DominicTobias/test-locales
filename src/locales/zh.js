b15aea31d0524cfa79520f759f88083d0dafb94f
Dominic Tobias <dominic.tobias@gmail.com>
2019-03-06 19:53:51 +0000

    Create new locale zh.js

---
+++ src/locales/zh.js
@@ -0,0 +1,18 @@
+/* eslint disable */
+const entries = {
+  country: [
+    '德国',
+    '捷克',
+    '英国',
+    '瑞典'
+  ],
+  'rather.long': 'Lorem ipsum dolor坐下来，精致的adipistur elit。 Sed hendrerit volptpat lorem，eu viverra neque tristique sit amet。 Vestibulum vulputate nunc vitae auctor ornare。 Pellentesque居民morbi tristique senectus et netus et malesuada fames ac turpis egestas。 Praesent blandit leo quis faucibus curo。',
+  'adding-apple': '苹果',
+  'adding-lemon': '柠檬',
+  'adding-orange': '橙子',
+  'keys-only-test': '仅限英语',
+  'global-nav-back': '回去',
+  'copy-value-to-all': '所有语言都应该相同'
+};
+
+export default entries;
